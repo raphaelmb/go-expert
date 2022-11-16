00a4590fc1c19f10c5e2834f69ce1fdd22bdfa83
Raphael Berger <raphaelmb@gmail.com>
2022-11-16 18:07:26 -0300

    API product routes

--- 7-api/cmd/server/main.go
+++ 7-api/cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/raphaelmb/go-expert/7-api/configs"
 	"github.com/raphaelmb/go-expert/7-api/internal/entity"
 	"github.com/raphaelmb/go-expert/7-api/internal/infra/database"
@@ -25,6 +27,13 @@ func main() {
 	productDB := database.NewProduct(db)
 	ProductHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", ProductHandler.CreateProduct)
-	http.ListenAndServe(":8080", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", ProductHandler.CreateProduct)
+	r.Get("/products", ProductHandler.GetAllProducts)
+	r.Get("/products/{id}", ProductHandler.GetProduct)
+	r.Put("/products/{id}", ProductHandler.UpdateProduct)
+	r.Delete("/products/{id}", ProductHandler.DeleteProduct)
+
+	http.ListenAndServe(":8080", r)
 }

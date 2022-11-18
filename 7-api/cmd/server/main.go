e09b73b19cfa4340d33c7341eecbfeceb178a236
Raphael Berger <raphaelmb@gmail.com>
2022-11-17 22:33:26 -0300

    test

--- 7-api/cmd/server/main.go
+++ 7-api/cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import "github.com/raphaelmb/go-expert/7-api/configs"
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
 }

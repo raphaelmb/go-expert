21d08b0f4a05932ba808317bccc83a478685853e
Raphael Berger <raphaelmb@gmail.com>
2023-01-16 10:14:32 -0300

    typo

--- 9-events/pkg/events/event_dispatcher_test.go
+++ 9-events/pkg/events/event_dispatcher_test.go
@@ -105,42 +105,44 @@ func (suite *EventDispatcherTestSuite) TestEventDispatch_Dispatch() {
 	suite.eventDispatcher.Dispatch(&suite.event)
 	eh.AssertExpectations(suite.T())
 	eh.AssertNumberOfCalls(suite.T(), "Handle", 1)
+}
+
 func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
-	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)	
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
 	suite.Nil(err)
 	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 
-	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)	
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
 	suite.Equal(ErrHandlerAlreadyRegistered, err)
 	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 }
 
 func (suite *EventDispatcherTestSuite) TestEventDispatcher_Clear() {
 	// Event 1
-	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)	
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
 	suite.Nil(err)
 	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 
-	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)	
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
 	suite.Nil(err)
 	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 
 	// Event 2
-	err = suite.eventDispatcher.Register(suite.event2.GetName(), &suite.handler3)	
+	err = suite.eventDispatcher.Register(suite.event2.GetName(), &suite.handler3)
 	suite.Nil(err)
 	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
-	
+
 	suite.eventDispatcher.Clear()
 	suite.Equal(0, len(suite.eventDispatcher.handlers))
 }
 
 func (suite *EventDispatcherTestSuite) TestEventDispatcher_Has() {
 	// Event 1
-	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)	
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
 	suite.Nil(err)
 	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 
-	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)	
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
 	suite.Nil(err)
 	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 
